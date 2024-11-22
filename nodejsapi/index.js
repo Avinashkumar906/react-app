9d6689b7f70803861e5e946a8e2b2817f434e4bc
Mr. Avinash <avinashkumar906@gmail.com>
2024-11-22 12:15:48 +0530

    updated path join

--- nodejsapi/index.js
+++ nodejsapi/index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
         res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
         res.header('Expires', '-1');
         res.header('Pragma', 'no-cache');
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../build', 'index.html'));
     }
 });
 app.use(express.static(path.join(__dirname,'../build')))
